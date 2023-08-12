9403d07051bedbb21464a48a55bc91294e940f1f
Seungchan0325 <imchan0325@gmail.com>
2023-08-12 22:22:42 +0900

    add query.txt

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
-pre_message = []
-def split_sentence(sentence: str) -> str:
-    message = pre_message
-    message.append({"role": "user", "content": f'""{sentence}""'})
-    print(message)
+def query(message: list[dict[str, str]]) -> str:
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=message
     )
-    print(response)
+    print("Debug: ", response)
+    return response['choices'][0]['message']['content']
 
-    text = response['choices'][0]['message']['content']
-    print(text)
-    return text 
+def splitSentence(sentence: str) -> list[str]:
+    message = [
+        {"role": "user", "content": ""},
+        {"role": "user", "content": f'""{sentence}""'}
+    ]
 
-sentence = input()
+    text = query(message)
 
-a = split_sentence(sentence)
-print(a)
+    return text
+
+def easyWord(sentence: str) -> str:
+    message = [
+        {"role": "user", "content": ""},
+        {"role": "user", "content": f'""{sentence}""'}
+    ]
+
+    text = query(message)
+
+    return text
+
+def easySentence(sentence: str) -> list[str]:
+    sentences = splitSentence(sentence)
+    
+    ret = []
+    for i in sentences:
+        ret.append(easyWord(i))
+    
+    return ret
+
+def main():
+    text = input()
+
+    result = easySentence(text)
+
+    print(result)
+
+if __name__ == '__main__':
+    main()
