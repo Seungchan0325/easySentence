fd534a97e30693d90ef9614a62f70fa7b2851d9b
Seungchan0325 <imchan0325@gmail.com>
2023-08-14 18:51:07 +0900

    add easySentecne.py

---
+++ easySentence.py
@@ -0,0 +1,69 @@
+from konlpy.tag import Kkma
+
+import openai
+import json
+
+class EasySentenceTranslator:
+    def __init__(self, api_key: str) -> None:
+        openai.api_key = api_key
+
+        with open('./simplify_sentence_prompt.txt', 'r', encoding='utf-8') as f:
+            self.simplify_sentence_prompt = f.read()
+
+        self.kkma = Kkma()
+
+    def query(self, prompt: str) -> str:
+        messages = [{'role': 'user', 'content': prompt}]
+
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=messages,
+            temperature=1
+        )
+
+        if response['choices'][0]['finish_reason'] != 'stop':
+            raise Exception("GPT didn't answer")
+
+        return response['choices'][0]['message']['content']
+
+    def parse_json(self, text: str) -> bool:
+        markdown = text.find('```')
+        start_idx = 0
+        if markdown != -1:
+            start_idx = markdown + 2
+        l = text.find('{', start_idx)
+        r = text.rfind('}') + 1
+        text = text[l:r]
+
+        try:
+            ret = json.loads(text)
+        except:
+            return -1
+
+        return ret
+
+    def simplifySentence(self, sentence: str) -> list[str]:
+        prompt = self.simplify_sentence_prompt + f'\n\n ```{sentence}```'
+
+        while True:
+            answer = self.query(prompt)
+            json_object = self.parse_json(answer)
+
+            if json_object == -1:
+                # print('again')
+                # print('prompt: ', prompt)
+                # print('answer: ', answer)
+                continue
+            else:
+                break
+
+        return json_object['sentences']
+
+    def translate(self, text: str) -> list[list[str]]:
+        sentences = self.kkma.sentences(text)
+
+        ret = []
+        for sentence in sentences:
+            ret.append(self.simplifySentence(sentence))
+        
+        return ret
--- main.py
+++ main.py
@@ -1,86 +1,17 @@
 import argparse
-import openai
-import json
-
-openai.api_key = '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'
-
-def query(prompt: str) -> str:
-    messages = [{'role': 'user', 'content': prompt}]
-
-    response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=messages,
-        temperature=1
-    )
-
-    if response['choices'][0]['finish_reason'] != 'stop':
-        print("Error: gpt didn't answer")
-        exit(-1)
-
-    return response['choices'][0]['message']['content']
-
-def parse_json(text: str) -> bool:
-    markdown = text.find('```')
-    start_idx = 0
-    if markdown != -1:
-        start_idx = markdown + 2
-    l = text.find('{', start_idx)
-    r = text.rfind('}') + 1
-    text = text[l:r]
-
-    try:
-        ret = json.loads(text)
-    except:
-        return -1
-
-    return ret
-
-def translateToEasySentences(sentence: str) -> list[str]:
-    prompt = translate_to_easy_sentences_prompt + f'\n\n ```{sentence}```'
-
-    while True:
-        answer = query(prompt)
-        json_object = parse_json(answer)
-
-        if json_object == -1:
-            print('again')
-            continue
-        else:
-            break
-
-    return json_object['sentences']
-
-def splitText(text: str) -> list[str]:
-    prompt = split_text_prompt + f'\n\n```{text}```'
-    
-    while True:
-        answer = query(prompt)
-        json_object = parse_json(answer)
-
-        if json_object == -1:
-            print('again1')
-            continue
-        else:
-            break
-
-    return json_object['sentences']
+import easySentence
 
 def main(args: argparse.Namespace):
     with open(args.input_file, 'r', encoding='utf-8') as f:
         text = f.read()
-    
-    global translate_to_easy_sentences_prompt
-    global split_text_prompt
-    with open('./translate_to_easy_sentences_prompt.txt', 'r', encoding='utf-8') as f:
-        translate_to_easy_sentences_prompt = f.read()
-    with open('./split_text_prompt.txt', 'r', encoding='utf-8') as f:
-        split_text_prompt = f.read()
 
-    sentences = splitText(text)
-    for sentence in sentences:
-        for easy_sentence in translateToEasySentences(sentence):
-            print(easy_sentence)
-        print('-'*50)
+    translator = easySentence.EasySentenceTranslator("API KEY")
+    
+    result = translator.translate(text)
+    for i in result:
+        for j in i:
+            print(j)
+        print('-' * 50)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
