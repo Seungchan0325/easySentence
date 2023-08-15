f5150a22225a9359c224bdd9a7e6299209b3f89b
Seungchan0325 <imchan0325@gmail.com>
2023-08-15 16:30:46 +0900

    add gui

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Easy Sentence Translator With AI")
 
-translator = easySentence.EasySentenceTranslator("OPENAI API KEY")
+translator = easySentence.EasySentenceTranslator("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")
 
 with st.form("form"):
     user_input = st.text_area("글을 입력해주세요 !")
@@ -13,6 +13,8 @@ if submit and user_input:
     with st.spinner("문장 변환중입니다. 잠시만 기다려주세요..."):
         result = translator.translate(user_input)
 
+    # markdown 표
+    st.write('-' * 50)
     for i in result:
         for j in i:
             st.write(j)
